627c88ec33a14e2a5d1c9636a9c499a69f4659bc
Aubrey M <moralesa2@my.lanecc.edu>
2024-10-14 16:24:22 -0700

    Lab 2 mostly complete, may need some final touches.

---
+++ MyCommunitySite/MyCommunitySite/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCommunitySite.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Links()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
--- MyCommunitySite/MyCommunitySite/Controllers/HomeController.cs
+++ MyCommunitySite/MyCommunitySite/Controllers/HomeController.cs
@@ -23,6 +23,18 @@ namespace MyCommunitySite.Controllers
             return View();
         }
 
+        public IActionResult History()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
