579dbd1bf11861ff85fd313ba3384942dae4ef64
Aubrey M <moralesa2@my.lanecc.edu>
2024-10-24 17:12:29 -0700

    Final changes made for lab 3. Added community-specific details

---
+++ MyCommunitySite/MyCommunitySite/Models/Hike.cs
@@ -0,0 +1,10 @@
+﻿namespace MyCommunitySite.Models
+{
+    public class Hike
+    {
+        public string Location { get; set; }
+        public string Date { get; set; }
+
+        /*public int Attending { get; set; } Implement this later*/
+    }
+}
--- MyCommunitySite/MyCommunitySite/Models/Message.cs
+++ MyCommunitySite/MyCommunitySite/Models/Message.cs
@@ -4,6 +4,8 @@
     {
         public AppUser? Sender { get; set; }
         public AppUser? Recipient { get; set; }
+        public string? Subject { get; set; }
+        public int Priority { get; set; }
         public string? Content { get; set; }
         public DateTime TimeSent { get; set; }
     }
