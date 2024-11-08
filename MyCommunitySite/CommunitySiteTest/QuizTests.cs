32e5e232c0fdacdeb47a4016c45edc93af7fbb04
Aubrey M <moralesa2@my.lanecc.edu>
2024-11-07 20:25:54 -0800

    Quiz project mostly complete, need to add more questions and finish styling.

---
+++ MyCommunitySite/CommunitySiteTest/QuizTests.cs
@@ -0,0 +1,38 @@
+using MyCommunitySite.HikingQuiz;
+
+namespace CommunitySiteTest;
+
+public class QuizTests
+{
+    private Quiz _quiz = new Quiz();
+
+    [Fact]
+    public void CheckCorrectAnswer()
+    {
+        Question q1 = new Question()
+        {
+            Q = "Who is the author of Pet Semetary?",
+            A = "Stephen King",
+            UserA = "stephen king"
+        };
+        _quiz.Questions.Add(q1);
+
+        bool isCorrect = _quiz.CheckAnswer(_quiz.Questions.Last());
+        Assert.True(isCorrect);
+    }
+
+    [Fact]
+    public void CheckIncorrectAnswer()
+    {
+        Question q2 = new Question()
+        {
+            Q = "Who is the author of the short story Moxon's Master?",
+            A = "Ambrose Beirce",
+            UserA = "Algernon Blackwood"
+        };
+        _quiz.Questions.Add(q2);
+
+        bool isCorrect = _quiz.CheckAnswer(_quiz.Questions.Last());
+        Assert.False(isCorrect);
+    }
+}
--- MyCommunitySite/MyCommunitySite/Controllers/HomeController.cs
+++ MyCommunitySite/MyCommunitySite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCommunitySite.HikingQuiz;
 using MyCommunitySite.Models;
 using System.Diagnostics;
 
@@ -26,6 +27,28 @@ namespace MyCommunitySite.Controllers
         public IActionResult History()
         {
             return View();
+        }
+
+        public IActionResult Quiz()
+        {
+            Quiz model = new Quiz();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Quiz(string[] answers)
+        {
+            Quiz model = new Quiz();
+            for (int i = 0; i < answers.Length; i++)
+            {
+                string answer = answers[i];
+                if (answer != null)
+                {
+                    Question question = model.Questions[i];
+                    question.UserA = answer;
+                    question.isCorrect = model.CheckAnswer(question);
+                }
+            }
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
